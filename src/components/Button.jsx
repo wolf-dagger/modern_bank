ce0bd99353b8cedcba58a05617ee020be0ac5bd5
wolf-dagger <demonwolfisback@gmail.com>
2024-03-09 22:21:26 +0530

    Billing and buisness section added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,14 @@
-
-const Button = () => {
+const Button = ({ styles }) => {
   return (
-    <div>button</div>
-  )
-}
+    <>
+      <button
+        type="button"
+        className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}
+      >
+        Get Started
+      </button>
+    </>
+  );
+};
 
-export default Button
+export default Button;
